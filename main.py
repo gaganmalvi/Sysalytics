90ca6f9fa920c9a048ee4834c2d2d73c75ef86bd
Gagan Malvi <malvigagan@gmail.com>
2022-06-03 21:06:39 +0530

    feat(push): Store data on Firestore.
    Signed-off-by: Gagan Malvi <malvigagan@gmail.com>

--- main.py
+++ main.py
@@ -7,25 +7,45 @@ This script pulls the following statistics.
 - CPU frequency
 - Swappiness
 '''
-import tools.io_ops as io
 import tools.sysnodes as nodes
+import firebase_admin
+from firebase_admin import db
+
+# Parse environment variables the easy way
+envVars = []
+with open('.env', 'r') as f:
+    envVars = f.read().splitlines()
+
+# Initialize Realtime Database
+databaseURL = envVars[0]
+cred = firebase_admin.credentials.Certificate(envVars[1])
+
+default_app = firebase_admin.initialize_app(
+    cred,
+    {"databaseURL": databaseURL}
+)
+
+# Set db reference to root
+dbRef = db.reference('/')
 
 # Read all data, write to JSON
 print("[*] Reading data...")
 
 data = {
-    "battery_capacity": nodes.BATTERY_CAPACITY,
-    "battery_model": nodes.BATTERY_MODEL,
-    "battery_status": nodes.BATTERY_STATUS,
+    "battery": {
+        "status": nodes.BATTERY_STATUS,
+        "capacity": nodes.BATTERY_CAPACITY,
+        "model": nodes.BATTERY_MODEL
+    },
     "cpu_num_cpus": nodes.num_cpus,
     "cpu_cur_freq": nodes.CPU_CUR_FREQ,
     "cpu_max_freq": nodes.CPU_MAX_FREQ,
     "cpu_min_freq": nodes.CPU_MIN_FREQ,
-    "cpu_scal_gov": nodes.CPU_CUR_SCALING_GOV,
+    "cpu_scal_gov": nodes.CPU_CUR_SGOV,
     "cpu_avl_sgov": nodes.CPU_AVL_SGOV,
     "swappiness": nodes.SWAPPINESS
 }
 
 # Write to JSON
-print("[*] Writing data to JSON...")
-io.write_json(data)
+print("[*] Writing data to Firestore...")
+dbRef.set(data)
--- tools/sysnodes.py
+++ tools/sysnodes.py
@@ -25,20 +25,23 @@ NODE_CPU_CUR_FREQ = []
 NODE_CPU_MAX_FREQ = []
 NODE_CPU_MIN_FREQ = []
 NODE_CPU_AVL_SGOV = []
+NODE_CPU_CUR_SGOV = []
 for i in range(num_cpus):
     NODE_CPU_CUR_FREQ.append("/sys/devices/system/cpu/cpu" + str(i) + "/cpufreq/scaling_cur_freq")
     NODE_CPU_MAX_FREQ.append("/sys/devices/system/cpu/cpu" + str(i) + "/cpufreq/scaling_max_freq")
     NODE_CPU_MIN_FREQ.append("/sys/devices/system/cpu/cpu" + str(i) + "/cpufreq/scaling_min_freq")
     NODE_CPU_AVL_SGOV.append("/sys/devices/system/cpu/cpu" + str(i) + "/cpufreq/scaling_available_governors")
+    NODE_CPU_CUR_SGOV.append("/sys/devices/system/cpu/cpu" + str(i) + "/cpufreq/scaling_governor")
 
 CPU_CUR_FREQ = []
 CPU_MAX_FREQ = []
 CPU_MIN_FREQ = []
-CPU_CUR_SCALING_GOV = []
-CPU_AVL_SGOV = io.read_node("/sys/devices/system/cpu/cpu0/cpufreq/scaling_available_governors") # Only one CPU node is enough to check for available governors
+CPU_AVL_SGOV = []
+CPU_CUR_SGOV = []
 
 for i in range(num_cpus):
     CPU_CUR_FREQ.append(io.read_node(NODE_CPU_CUR_FREQ[i]))
     CPU_MAX_FREQ.append(io.read_node(NODE_CPU_MAX_FREQ[i]))
     CPU_MIN_FREQ.append(io.read_node(NODE_CPU_MIN_FREQ[i]))
-    CPU_CUR_SCALING_GOV.append(io.read_node(NODE_CPU_AVL_SGOV[i]))
+    CPU_AVL_SGOV.append(io.read_node(NODE_CPU_AVL_SGOV[i]))
+    CPU_CUR_SGOV.append(io.read_node(NODE_CPU_CUR_SGOV[i]))
